03d3cd15620bb26ee8c551634177112b3123ee70
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-03-08 16:12:58 -0300

    tipos de datos mas comunes

---
+++ Variables.js
@@ -0,0 +1,27 @@
+//String
+
+var bootcamp = 'Henry';
+
+//Numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined (sin valor asignado en la variable)
+
+var cajon1;
+
+console.log(cajon1); 
+
+//Null
+
+var sinNada = null;
